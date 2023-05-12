224de8668fe6448c13180cde15edc8028d10ba33
NathanNyawaya <nyawayanathan@gmail.com>
2023-05-13 01:25:50 +0300

    semi final updates

---
+++ src/components/constants.js
@@ -0,0 +1,29 @@
+import { bg, bf2 } from "../images"
+
+export const List = [
+    {
+        image: `${bg}`,
+        title: "One",
+        id: 1
+    },
+    {
+        image: `${bf2}`,
+        title: "two",
+        id: 2
+    },
+    {
+        image: `${bg}`,
+        title: "three",
+        id: 3
+    },
+    {
+        image: `${bf2}`,
+        title: "four",
+        id: 4
+    },
+    {
+        image: `${bg}`,
+        title: "five",
+        id: 5
+    },
+]
---
+++ src/images/index.js
@@ -0,0 +1,17 @@
+import add from './add.png'
+import bg from './bg.avif'
+import bf2 from './bg.png'
+import initial from './initial.png'
+import movie from './movie.jpg'
+import remove from './remove.png'
+import star from './star.png'
+
+export {
+    add,
+    bg,
+    bf2,
+    initial,
+    movie,
+    remove,
+    star
+}
